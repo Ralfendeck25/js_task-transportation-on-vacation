ff7cf257f8eac59062fa5eb856af361a2d069ece
Ralfendeck25 <poseidoncenghizerzhemetmhossad@outlook.com>
2025-03-27 12:59:35 -0300

    Solution

---
+++ src/calculateRentalCost.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} days
+ *
+ * @return {number}
+ */
+function calculateRentalCost(days) {
+  const baseCost = days * 40;
+  if (days >= 7) {
+    return baseCost - 50;
+  } else if (days >= 3) {
+    return baseCost - 20;
+  } else {
+    return baseCost;
+  }
+}
+
+module.exports = calculateRentalCost;
